b9cd83a823a7df0b8bea835a9dbbf958b65f23cf
Git User <git@petra.ithemes.com>
2013-12-13 16:46:49 -0600

    1.0.10: updated: lib/updater

--- exchange-addon-membership.php
+++ exchange-addon-membership.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Membership Add-on
- * Version: 1.0.9
+ * Version: 1.0.10
  * Description: Adds the membership management to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/membership/
  * Author: iThemes
