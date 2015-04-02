bd3927c1ed54719d91e4cdce2e1cdb51b3ecf1bb
Lew Ayotte <lew@ithemes.com>
2015-04-02 13:26:47 -0500

    Tagging 1.2.31

--- exchange-addon-membership.php
+++ exchange-addon-membership.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Membership Add-on
- * Version: 1.2.30
+ * Version: 1.2.31
  * Description: Adds the membership management to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/membership/
  * Author: iThemes
