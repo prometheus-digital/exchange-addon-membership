77be0bbfab7fe4929bced41bbb6fe4a2ad08c943
Lew Ayotte <lew@ithemes.com>
2015-04-20 08:47:25 -0500

    Tagging 1.3.0 for esc_url() update

--- exchange-addon-membership.php
+++ exchange-addon-membership.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Membership Add-on
- * Version: 1.2.31
+ * Version: 1.3.0
  * Description: Adds the membership management to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/membership/
  * Author: iThemes
