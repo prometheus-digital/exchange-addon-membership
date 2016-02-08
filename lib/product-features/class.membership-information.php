39849b556df5470dfca8949913b7df535f057562
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-02-08 12:20:44 -0500

    Hide membership-information settings behind checkboxes
    We'll be adding custom restriction messages to this tab. Since those settings
    don't seem to be often used, we can hide them behind checkboxes.

--- lib/product-features/class.membership-information.php
+++ lib/product-features/class.membership-information.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
  * This will control membership welcome messages on the frontend membership dashboard
  *
- * @since 1.1.0
+ * @since   1.1.0
  * @package IT_Exchange_Addon_Membership
-*/
-
-
+ */
 class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 
 	/**
 	 * Constructor. Registers hooks
 	 *
 	 * @since 1.1.0
-	 * @return void
-	*/
+	 */
 	function __construct() {
 		if ( is_admin() ) {
 			add_action( 'load-post-new.php', array( $this, 'init_feature_metaboxes' ) );
@@ -24,8 +22,14 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 		add_action( 'it_exchange_enabled_addons_loaded', array( $this, 'add_feature_support_to_product_types' ) );
 		add_action( 'it_exchange_update_product_feature_membership-information', array( $this, 'save_feature' ), 9, 3 );
 		add_filter( 'it_exchange_get_product_feature_membership-information', array( $this, 'get_feature' ), 9, 3 );
-		add_filter( 'it_exchange_product_has_feature_membership-information', array( $this, 'product_has_feature') , 9, 3 );
-		add_filter( 'it_exchange_product_supports_feature_membership-information', array( $this, 'product_supports_feature') , 9, 2 );
+		add_filter( 'it_exchange_product_has_feature_membership-information', array(
+			$this,
+			'product_has_feature'
+		), 9, 3 );
+		add_filter( 'it_exchange_product_supports_feature_membership-information', array(
+			$this,
+			'product_supports_feature'
+		), 9, 2 );
 	}
 
 	/**
@@ -33,7 +37,7 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	 *
 	 * @since 1.1.0
 	 * @return void
-	*/
+	 */
 	function IT_Exchange_Addon_Membership_Product_Feature_Membership_Information() {
 		self::__construct();
 	}
@@ -42,7 +46,7 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	 * Register the product feature and add it to enabled product-type addons
 	 *
 	 * @since 1.1.0
-	*/
+	 */
 	function add_feature_support_to_product_types() {
 		// Register the product feature
 		$slug        = 'membership-information';
@@ -57,50 +61,61 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	 *
 	 * @since 1.1.0
 	 * @return void
-	*/
+	 */
 	function init_feature_metaboxes() {
-		
+
 		global $post;
-		
+
 		if ( isset( $_REQUEST['post_type'] ) ) {
 			$post_type = $_REQUEST['post_type'];
 		} else {
-			if ( isset( $_REQUEST['post'] ) )
+			if ( isset( $_REQUEST['post'] ) ) {
 				$post_id = (int) $_REQUEST['post'];
-			elseif ( isset( $_REQUEST['post_ID'] ) )
+			} elseif ( isset( $_REQUEST['post_ID'] ) ) {
 				$post_id = (int) $_REQUEST['post_ID'];
-			else
+			} else {
 				$post_id = 0;
+			}
 
-			if ( $post_id )
+			if ( $post_id ) {
 				$post = get_post( $post_id );
+			}
 
-			if ( isset( $post ) && !empty( $post ) )
+			if ( isset( $post ) && ! empty( $post ) ) {
 				$post_type = $post->post_type;
+			}
 		}
-			
-		if ( !empty( $_REQUEST['it-exchange-product-type'] ) )
+
+		if ( ! empty( $_REQUEST['it-exchange-product-type'] ) ) {
 			$product_type = $_REQUEST['it-exchange-product-type'];
-		else
+		} else {
 			$product_type = it_exchange_get_product_type( $post );
-		
-		if ( !empty( $post_type ) && 'it_exchange_prod' === $post_type ) {
-			if ( !empty( $product_type ) &&  it_exchange_product_type_supports_feature( $product_type, 'membership-information' ) )
-				add_action( 'it_exchange_product_metabox_callback_' . $product_type, array( $this, 'register_metabox' ), 1 ); //we want this to appear first in Membership product types
 		}
-		
+
+		if ( ! empty( $post_type ) && 'it_exchange_prod' === $post_type ) {
+			if ( ! empty( $product_type ) && it_exchange_product_type_supports_feature( $product_type, 'membership-information' ) ) {
+				add_action( 'it_exchange_product_metabox_callback_' . $product_type, array(
+					$this,
+					'register_metabox'
+				), 1 );
+			} //we want this to appear first in Membership product types
+		}
+
 	}
 
 	/**
 	 * Registers the feature metabox for a specific product type
 	 *
-	 * Hooked to it_exchange_product_metabox_callback_[product-type] where product type supports the feature 
+	 * Hooked to it_exchange_product_metabox_callback_[product-type] where product type supports the feature
 	 *
 	 * @since 1.1.0
 	 * @return void
-	*/
+	 */
 	function register_metabox() {
-		add_meta_box( 'it-exchange-product-membership-information', __( 'Membership Information', 'LION' ), array( $this, 'print_metabox' ), 'it_exchange_prod', 'it_exchange_advanced' );
+		add_meta_box( 'it-exchange-product-membership-information', __( 'Membership Information', 'LION' ), array(
+			$this,
+			'print_metabox'
+		), 'it_exchange_prod', 'it_exchange_advanced' );
 	}
 
 	/**
@@ -108,27 +123,75 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	 *
 	 * @since 1.1.0
 	 * @return void
-	*/
+	 */
 	function print_metabox( $post ) {
 		// Grab the iThemes Exchange Product object from the WP $post object
-		$product = it_exchange_get_product( $post );
-        $defaults = it_exchange_get_option( 'addon_membership' );
-		
+		$product  = it_exchange_get_product( $post );
+		$defaults = it_exchange_get_option( 'addon_membership' );
+
 		$intended_audience = it_exchange_get_product_feature( $product->ID, 'membership-information', array( 'setting' => 'intended-audience' ) );
-		$objectives = it_exchange_get_product_feature( $product->ID, 'membership-information', array( 'setting' => 'objectives' ) );
-		$prerequisites = it_exchange_get_product_feature( $product->ID, 'membership-information', array( 'setting' => 'prerequisites' ) );
-		
-		echo '<p><label for="it-exchange-membership-information-intended-audience-template" class="customer-information-label">' . $defaults['membership-intended-audience-label'] . ' <span class="tip" title="' . __('This label will appear when displaying the intended audience information on a membership.', 'LION' ) . '">i</span></label></p>';
-        
-        echo wp_editor( $intended_audience, 'membership-information-intended-audience-template', array( 'textarea_name' => 'it-exchange-membership-information-intended-audience-template', 'textarea_rows' => 10, 'textarea_cols' => 30, 'editor_class' => 'large-text', 'teeny' => true ) );
-		
-		echo '<p><label for="it-exchange-membership-information-objectives-template" class="customer-information-label">' . $defaults['membership-objectives-label'] . ' <span class="tip" title="' . __('This label will appear when displaying the objective information on a membership.', 'LION' ) . '">i</span></label></p>';
-		
-        echo wp_editor( $objectives, 'membership-information-objectives-template', array( 'textarea_name' => 'it-exchange-membership-information-objectives-template', 'textarea_rows' => 10, 'textarea_cols' => 30, 'editor_class' => 'large-text', 'teeny' => true ) );
-		
-		echo '<p><label for="it-exchange-membership-information-prerequisites-template" class="customer-information-label">' . $defaults['membership-prerequisites-label'] . ' <span class="tip" title="' . __('This label will appear when displaying the prerequisite information on a membership.', 'LION' ) . '">i</span></label></p>';
-		
-        echo wp_editor( $prerequisites, 'membership-information-prerequisites-template', array( 'textarea_name' => 'it-exchange-membership-information-prerequisites-template', 'textarea_rows' => 10, 'textarea_cols' => 30, 'editor_class' => 'large-text', 'teeny' => true ) );
+		$objectives        = it_exchange_get_product_feature( $product->ID, 'membership-information', array( 'setting' => 'objectives' ) );
+		$prerequisites     = it_exchange_get_product_feature( $product->ID, 'membership-information', array( 'setting' => 'prerequisites' ) );
+		?>
+
+		<p>
+			<label for="it-exchange-show-intended-audience" class="customer-information-label">
+
+				<input type="checkbox" id="it-exchange-show-intended-audience" <?php checked( ! empty( $intended_audience ) ); ?>>&nbsp;
+
+				<?php printf( __( 'Display %s', 'LION' ), $defaults['membership-intended-audience-label'] ); ?>
+			</label>
+		</p>
+
+		<div <?php echo empty( $intended_audience ) ? 'class="hide-if-js"' : ''; ?> id="it-exchange-intended-audience">
+			<?php wp_editor( $intended_audience, 'membership-information-intended-audience-template', array(
+				'textarea_name' => 'it-exchange-membership-information-intended-audience-template',
+				'textarea_rows' => 5,
+				'textarea_cols' => 30,
+				'editor_class'  => 'large-text',
+				'teeny'         => true
+			) ); ?>
+		</div>
+
+		<p>
+			<label for="it-exchange-show-objectives" class="customer-information-label">
+
+				<input type="checkbox" id="it-exchange-show-objectives" <?php checked( ! empty( $objectives ) ); ?>>&nbsp;
+
+				<?php printf( __( 'Display %s', 'LION' ), $defaults['membership-objectives-label'] ); ?>
+			</label>
+		</p>
+
+		<div <?php echo empty( $objectives ) ? 'class="hide-if-js"' : ''; ?> id="it-exchange-objectives">
+			<?php wp_editor( $objectives, 'membership-information-objectives-template', array(
+				'textarea_name' => 'it-exchange-membership-information-objectives-template',
+				'textarea_rows' => 5,
+				'textarea_cols' => 30,
+				'editor_class'  => 'large-text',
+				'teeny'         => true
+			) ); ?>
+		</div>
+
+		<p>
+			<label for="it-exchange-show-prerequisites" class="customer-information-label">
+
+				<input type="checkbox" id="it-exchange-show-prerequisites" <?php checked( ! empty( $prerequisites ) ); ?>>&nbsp;
+
+				<?php printf( __( 'Display %s', 'LION' ), $defaults['membership-prerequisites-label'] ); ?>
+			</label>
+		</p>
+
+		<div <?php echo empty( $prerequisites ) ? 'class="hide-if-js"' : ''; ?> id="it-exchange-prerequisites">
+			<?php wp_editor( $prerequisites, 'membership-information-prerequisites-template', array(
+				'textarea_name' => 'it-exchange-membership-information-prerequisites-template',
+				'textarea_rows' => 5,
+				'textarea_cols' => 30,
+				'editor_class'  => 'large-text',
+				'teeny'         => true
+			) ); ?>
+		</div>
+
+		<?php
 	}
 
 	/**
@@ -137,49 +200,58 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	 * @since 1.1.0
 	 *
 	 * @param object $post wp post object
+	 *
 	 * @return void
-	*/
+	 */
 	function save_feature_on_product_save() {
 		// Abort if we can't determine a product type
-		if ( ! $product_type = it_exchange_get_product_type() )
+		if ( ! $product_type = it_exchange_get_product_type() ) {
 			return;
+		}
 
 		// Abort if we don't have a product ID
 		$product_id = empty( $_POST['ID'] ) ? false : $_POST['ID'];
-		if ( ! $product_id )
+		if ( ! $product_id ) {
 			return;
+		}
 
 		// Abort if this product type doesn't support this feature 
-		if ( ! it_exchange_product_type_supports_feature( $product_type, 'membership-information' ) )
+		if ( ! it_exchange_product_type_supports_feature( $product_type, 'membership-information' ) ) {
 			return;
+		}
 
-		if ( empty( $_POST['it-exchange-membership-information-intended-audience-template'] ) )
+		if ( empty( $_POST['it-exchange-membership-information-intended-audience-template'] ) ) {
 			delete_post_meta( $product_id, '_it-exchange-product-membership-intended-audience' );
-		else
+		} else {
 			it_exchange_update_product_feature( $product_id, 'membership-information', $_POST['it-exchange-membership-information-intended-audience-template'], array( 'setting' => 'intended-audience' ) );
-			
-		if ( empty( $_POST['it-exchange-membership-information-objectives-template'] ) )
+		}
+
+		if ( empty( $_POST['it-exchange-membership-information-objectives-template'] ) ) {
 			delete_post_meta( $product_id, '_it-exchange-product-membership-objectives' );
-		else
+		} else {
 			it_exchange_update_product_feature( $product_id, 'membership-information', $_POST['it-exchange-membership-information-objectives-template'], array( 'setting' => 'objectives' ) );
-			
-		if ( empty( $_POST['it-exchange-membership-information-prerequisites-template'] ) )
+		}
+
+		if ( empty( $_POST['it-exchange-membership-information-prerequisites-template'] ) ) {
 			delete_post_meta( $product_id, '_it-exchange-product-membership-prerequisites' );
-		else
+		} else {
 			it_exchange_update_product_feature( $product_id, 'membership-information', $_POST['it-exchange-membership-information-prerequisites-template'], array( 'setting' => 'prerequisites' ) );
+		}
 	}
 
 	/**
 	 * Return the product's features
 	 *
 	 * @since 1.1.0
+	 *
 	 * @param mixed $existing the values passed in by the WP Filter API. Ignored here.
-	 * @param integer product_id the WordPress post ID
+	 * @param       integer   product_id the WordPress post ID
+	 *
 	 * @return string product feature
-	*/
-	function save_feature( $product_id, $new_value, $options=array() ) {
+	 */
+	function save_feature( $product_id, $new_value, $options = array() ) {
 		switch ( $options['setting'] ) {
-			
+
 			case 'intended-audience':
 				update_post_meta( $product_id, '_it-exchange-product-membership-intended-audience', $new_value );
 				break;
@@ -189,8 +261,9 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 			case 'prerequisites':
 				update_post_meta( $product_id, '_it-exchange-product-membership-prerequisites', $new_value );
 				break;
-			
+
 		}
+
 		return true;
 	}
 
@@ -200,12 +273,13 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	 * @since 1.1.0
 	 *
 	 * @param mixed $existing the values passed in by the WP Filter API. Ignored here.
-	 * @param integer product_id the WordPress post ID
+	 * @param       integer   product_id the WordPress post ID
+	 *
 	 * @return string product feature
-	*/
-	function get_feature( $existing, $product_id, $options=array() ) {
+	 */
+	function get_feature( $existing, $product_id, $options = array() ) {
 		switch ( $options['setting'] ) {
-			
+
 			case 'intended-audience':
 				return get_post_meta( $product_id, '_it-exchange-product-membership-intended-audience', true );
 			case 'objectives':
@@ -213,7 +287,7 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 			case 'prerequisites':
 				return get_post_meta( $product_id, '_it-exchange-product-membership-prerequisites', true );
 		}
-		
+
 		return false;
 	}
 
@@ -221,14 +295,17 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	 * Does the product have the feature?
 	 *
 	 * @since 1.1.0
-	 * @param mixed $result Not used by core
+	 *
+	 * @param mixed   $result Not used by core
 	 * @param integer $product_id
+	 *
 	 * @return boolean
-	*/
-	function product_has_feature( $result, $product_id, $options=array() ) {
+	 */
+	function product_has_feature( $result, $product_id, $options = array() ) {
 		// Does this product type support this feature?
-		if ( false === $this->product_supports_feature( false, $product_id, $options ) )
+		if ( false === $this->product_supports_feature( false, $product_id ) ) {
 			return false;
+		}
 
 		// If it does support, does it have it?
 		return (boolean) $this->get_feature( false, $product_id, $options );
@@ -237,21 +314,25 @@ class IT_Exchange_Addon_Membership_Product_Feature_Membership_Information {
 	/**
 	 * Does the product support this feature?
 	 *
-	 * This is different than if it has the feature, a product can 
+	 * This is different than if it has the feature, a product can
 	 * support a feature but might not have the feature set.
 	 *
 	 * @since 1.1.0
-	 * @param mixed $result Not used by core
+	 *
+	 * @param mixed   $result Not used by core
 	 * @param integer $product_id
+	 *
 	 * @return boolean
-	*/
+	 */
 	function product_supports_feature( $result, $product_id ) {
 		// Does this product type support this feature?
 		$product_type = it_exchange_get_product_type( $product_id );
-		if ( ! it_exchange_product_type_supports_feature( $product_type, 'membership-information' ) )
+		if ( ! it_exchange_product_type_supports_feature( $product_type, 'membership-information' ) ) {
 			return false;
+		}
 
 		return true;
 	}
 }
+
 $IT_Exchange_Addon_Membership_Product_Feature_Membership_Information = new IT_Exchange_Addon_Membership_Product_Feature_Membership_Information();
