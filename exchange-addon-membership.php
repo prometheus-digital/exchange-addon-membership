38f1e15c8d52fc6a07a24a781cb60f160c6bf900
root <root@petra.ithemes.com>
2016-03-21 09:17:27 -0500

    1.19.9: updated: bundled-addons/exchange-addon-recurring-payments

--- exchange-addon-membership.php
+++ exchange-addon-membership.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Membership Add-on
- * Version: 1.19.8
+ * Version: 1.19.9
  * Description: Adds the membership management to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/membership/
  * Author: iThemes
