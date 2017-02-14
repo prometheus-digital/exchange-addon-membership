5dea31c7138c589c203b08c9f4ea6d6e6cdda0a1
Timothy Jacobs <timothy@ithemes.com>
2017-02-14 18:03:19 -0500

    Fix loading of umbrella memberships

--- umbrella-memberships/load.php
+++ umbrella-memberships/load.php
@@ -6,27 +6,24 @@
  * @license GPLv2
  */
 
-add_action( 'plugins_loaded', function () {
+if ( class_exists( 'ITEGMS\Plugin' ) ) {
 
-	if ( class_exists( 'ITEGMS\Plugin' ) ) {
+    require_once ABSPATH . '/wp-admin/includes/plugin.php';
 
-		require_once ABSPATH . '/wp-admin/includes/plugin.php';
+    if ( is_plugin_active( 'exchange-addon-umbrella-memberships/exchange-addon-umbrella-memberships.php' ) ) {
+        deactivate_plugins( array( 'exchange-addon-umbrella-memberships/exchange-addon-umbrella-memberships.php' ) );
 
-		if ( is_plugin_active( 'exchange-addon-umbrella-memberships/exchange-addon-umbrella-memberships.php' ) ) {
-			deactivate_plugins( array( 'exchange-addon-umbrella-memberships/exchange-addon-umbrella-memberships.php' ) );
+        add_action( 'admin_notices', function () {
 
-			add_action( 'admin_notices', function () {
+            ?>
 
-				?>
+            <div class="notice notice-info">
+                <p><?php _e( 'Umbrella Memberships is now a part of the core iThemes Exchange Membership add-on. We\'ve deactivated the old add-on.', 'LION' ); ?></p>
+            </div>
 
-				<div class="notice notice-info">
-					<p><?php _e( 'Umbrella Memberships is now a part of the core iThemes Exchange Membership add-on. We\'ve deactivated the old add-on.', 'LION' ); ?></p>
-				</div>
-
-				<?php
-			} );
-		}
-	} else {
-		require_once __DIR__ . '/exchange-addon-umbrella-memberships.php';
-	}
-}, - 100 );
+            <?php
+        } );
+    }
+} else {
+    require_once __DIR__ . '/exchange-addon-umbrella-memberships.php';
+}
